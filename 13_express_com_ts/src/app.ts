f8873fef7870877545f4d393b5d9ebe8ba3b89d6
matlencar <rm94642@fiap.com.br>
2023-05-25 11:21:59 -0300

    subindo aula express com typescript

---
+++ 13_express_com_ts/src/app.ts
@@ -0,0 +1,55 @@
+// 1 - iniciando projeto
+
+// console.log("Express + TS, comecando")
+
+// 2 - init express
+import express, { Request, Response } from 'express'
+
+const app = express();
+
+// 3 - rota com POST
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.send("Hello Express")
+})
+
+// 3 - rota com POST
+app.post("/api/product", (req,res) => {
+
+    console.log(req.body);
+
+    return res.send("Produto adicionado !!!");
+})
+
+// 4 - rota para todos os verbos
+app.all("/api/product/check", (req, res) => {
+
+
+    // req.method = VERBO HTTP
+
+    if(req.method === "POST") {
+        return res.send("Inseriu algum registro");
+    } else if (req.method === "GET") {
+        return res.send("Leu algum registro");
+    } else {
+       return res.send("Não podemos realizar essa operação");
+    }
+});
+
+// 5 - interfaces  do express
+
+app.get("/api/interfaces", (req: Request, res: Response) => {
+
+
+    return res.send("Utilizando as interfaces");
+});
+
+
+// 6 - enviando JSON
+
+
+
+app.listen(3000, () => {
+    console.log("Aplicação de TS + Expres funcionado");
+});
